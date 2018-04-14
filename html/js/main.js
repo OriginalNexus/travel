25c75b8a2ad66103adc2870dac824fbce57f11da
Cursi <razvan.diib@yahoo.com>
2018-04-14 17:29:10 +0300

    Added responsive UI for platform

--- html/js/main.js
+++ html/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function()
 {
-  
+	$(".homeWrapper").addClass("activeSection");
+
+	$(".flexedContainer").click(function(event)
+	{
+		if(!$(event.target).hasClass("userName") && !$(event.target).hasClass("userImage") &&
+			 !$(event.target).hasClass("appLogo") && !$(event.target).hasClass("appTitle") &&
+		   !$(event.target).hasClass("leftHeader") && !$(event.target).hasClass("middleHeader"))
+		{
+			$(".flexedContainer").each(function()
+			{
+				$(this).removeClass("activeSection");
+			});
+
+			$(this).addClass("activeSection");
+		}
+	});
 });
